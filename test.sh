d90c68dea7232815b689d1a89557a1f4c7123d3d
Keenauto <yuru@Yurus-MacBook-Air.local>
2024-06-11 23:47:26 +0800

    update

--- test.sh
+++ test.sh
@@ -1,24 +1,4 @@
-apt-get install autoconf make gcc openjdk-11-jdk-headless gnupg curl -y
-
-wget https://archive.apache.org/dist/commons/daemon/source/commons-daemon-1.3.4-src.tar.gz
-
-tar zxvf commons-daemon-1.3.4-src.tar.gz
-
-cd commons-daemon-1.3.4-src/src/native/unix
-
-sh support/buildconf.sh
-
-./configure --with-java=/usr/lib/jvm/java-11-openjdk-amd64
-
-make
-
-ln -s ~/commons-daemon-1.2.4-src/src/native/unix/jsvc /usr/bin/
-
-curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
-
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-
-apt-get install mongodb-org mongodb-org-database mongodb-org-server mongodb-mongosh mongodb-org-mongos mongodb-org-tools -y
+apt-get install openjdk-8-jre-headless jsvc curl -y
 
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
 
@@ -26,8 +6,13 @@ dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 apt-get install mongodb-org -y
+
+wget https://static.tp-link.com/upload/software/2024/202402/20240227/Omada_SDN_Controller_v5.13.30.8_linux_x64.deb
+
+dpkg -i Omada_SDN_Controller_v5.13.30.8_Linux_x64.deb
+
 
 
