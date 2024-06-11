d0c997e149f85e99a654ea1267028a51063b7516
Keenauto <yuru@Yurus-MacBook-Air.local>
2024-06-11 20:45:58 +0800

    update

--- Unifi_install.sh
+++ Unifi_install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Install base needs and java 17
-sudo apt-get update && sudo apt-get install ca-certificates apt-transport-https openjdk-8-jre-headless jsvc gpg -y
+sudo apt-get update && sudo apt-get install ca-certificates apt-transport-https openjdk-17-jre-headless gpg -y
 
 # Add Ubiquiti & Mongo to the sources
 echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
