b42de94b73db2691f8486efd7c2fe556fd42151c
Keenauto <yuru@Yurus-MacBook-Air.local>
2024-06-11 20:45:09 +0800

    update

--- Unifi_install.sh
+++ Unifi_install.sh
@@ -11,13 +11,5 @@ echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gp
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
 sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
 
-# Download the latest TP-Link Omada software controller
-sudo wget https://static.tp-link.com/upload/software/2024/202402/20240227/Omada_SDN_Controller_v5.13.30.8_linux_x64.tar.gz
-
 # Install the UniFi controller
 sudo apt-get update && sudo apt-get install unifi -y
-
-
-tar xvf Omada_SDN_Controller_v5.13.30.8_linux_x64.tar.gz
-cd Omada_SDN_Controller_v5.13.30.8_linux_x64
-./install.sh
