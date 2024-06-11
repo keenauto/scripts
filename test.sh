24ad96506cf6182c33da93e3c40c6d734ff2cd86
Keenauto <yuru@Yurus-MacBook-Air.local>
2024-06-11 22:20:09 +0800

    update

--- test.sh
+++ test.sh
@@ -1,13 +1,33 @@
-# Install base needs and java 17
-sudo apt-get update && sudo apt-get install openjdk-8-jdk jsvc gpg -y
+apt-get install autoconf make gcc openjdk-11-jdk-headless gnupg curl -y
+
+wget https://archive.apache.org/dist/commons/daemon/source/commons-daemon-1.3.4-src.tar.gz
+
+tar zxvf commons-daemon-1.3.4-src.tar.gz
+
+cd commons-daemon-1.3.4-src/src/native/unix
+
+sh support/buildconf.sh
+
+./configure --with-java=/usr/lib/jvm/java-11-openjdk-amd64
+
+make
+
+ln -s ~/commons-daemon-1.2.4-src/src/native/unix/jsvc /usr/bin/
 
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
 
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
-# Download the latest TP-Link Omada software controller
-sudo wget https://static.tp-link.com/upload/software/2024/202402/20240227/Omada_SDN_Controller_v5.13.30.8_linux_x64.tar.gz
+apt-get install mongodb-org mongodb-org-database mongodb-org-server mongodb-mongosh mongodb-org-mongos mongodb-org-tools -y
+
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
+
+dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+apt-get install mongodb-org -y
 
 
-tar xvf Omada_SDN_Controller_v5.13.30.8_linux_x64.tar.gz
-cd Omada_SDN_Controller_v5.13.30.8_linux_x64
-./install.sh -y
